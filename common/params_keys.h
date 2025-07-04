cb5864500ce8a1e448c6564bf7654a0990058521
Teal <teal@teallvbs.xyz>
2025-07-04 12:42:55 -0500

    bugfix toggle

--- common/params_keys.h
+++ common/params_keys.h
@@ -195,4 +195,7 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"OsmStateTitle", PERSISTENT},
     {"OsmWayTest", PERSISTENT},
     {"RoadName", CLEAR_ON_ONROAD_TRANSITION},
+
+    // VW PQ PLA/HCA Toggle
+     {"pqLatControlToggle", PERSISTENT | BACKUP},
 };
