6e791e563294185363ff0ddd4142ed7bd775f2f0
Teal <teal@teallvbs.xyz>
2025-08-03 22:48:20 -0500

    it fixed it!!!!!

--- selfdrive/ui/soundd.py
+++ selfdrive/ui/soundd.py
@@ -30,7 +30,7 @@ AudibleAlert = car.CarControl.HUDControl.AudibleAlert
 sound_list: dict[int, tuple[str, int | None, float]] = {
   # AudibleAlert, file name, play count (none for infinite)
   AudibleAlert.engage: ("engage.wav", 1, MAX_VOLUME),
-  AudibleAlert.disengage: ("disengage.wav", 1, MAX_VOLUME),
+  AudibleAlert.disengage: ("disengage_airbus.wav", 1, MAX_VOLUME),
   AudibleAlert.refuse: ("refuse.wav", 1, MAX_VOLUME),
 
   AudibleAlert.prompt: ("prompt.wav", 1, MAX_VOLUME),
