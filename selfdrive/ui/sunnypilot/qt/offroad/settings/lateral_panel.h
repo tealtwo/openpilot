7bf7233ae06e156a3a77f40c6b8488622eb12e08
Teal <teal@teallvbs.xyz>
2025-09-02 23:44:16 -0500

    VW PQ: add UI toggle for HCA Status 5 & 7

--- selfdrive/ui/sunnypilot/qt/offroad/settings/lateral_panel.h
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/lateral_panel.h
@@ -40,6 +40,7 @@ private:
 
   ParamControl *madsToggle;
   ParamControl *pqLateralToggle;
+  ParamControl *pqhca5or7Toggle*;
   PushButtonSP *madsSettingsButton;
   MadsSettings *madsWidget = nullptr;
   PushButtonSP *laneChangeSettingsButton;
