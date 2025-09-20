9afadbbff507168080695b39e042114af845d676
teal <teal@teallvbs.xyz>
2025-09-20 13:02:31 -0500

    merge 09-20-2025

--- common/params_keys.h
+++ common/params_keys.h
@@ -226,6 +226,11 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"OsmWayTest", {PERSISTENT, STRING}},
     {"RoadName", {CLEAR_ON_ONROAD_TRANSITION, STRING}},
 
+    // Speed Limit
+    {"SpeedLimitOffsetType", {PERSISTENT | BACKUP, INT, "0"}},
+    {"SpeedLimitPolicy", {PERSISTENT | BACKUP, INT, "3"}},
+    {"SpeedLimitValueOffset", {PERSISTENT | BACKUP, INT, "0"}},
+
     // VW PQ:
     {"pqLatControlToggle", {PERSISTENT | BACKUP}}, // PLA/HCA Toggle
     {"eEPBopLongToggle", {PERSISTENT | BACKUP}}, // opLong eEPB Toggle
