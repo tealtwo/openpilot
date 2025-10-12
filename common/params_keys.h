6a103f4462ddf29fdb52a35663d304fb4cf517eb
Teal <teal@teallvbs.xyz>
2025-10-12 12:51:04 -0500

    NAVd: - add missing parameter keys to params_keys.h

--- common/params_keys.h
+++ common/params_keys.h
@@ -268,4 +268,8 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"NavigationDestination", {CLEAR_ON_MANAGER_START | CLEAR_ON_OFFROAD_TRANSITION, STRING}},
     {"NavigationActive", {CLEAR_ON_MANAGER_START | CLEAR_ON_OFFROAD_TRANSITION, BOOL}},
     {"NavigationSpeedControl", {PERSISTENT | BACKUP, BOOL, "1"}},
+    {"NavigationPreferences", {PERSISTENT | BACKUP, STRING}},
+    {"NavigationRouteAlternatives", {CLEAR_ON_MANAGER_START | CLEAR_ON_OFFROAD_TRANSITION, STRING}},
+    {"NavigationRouteSelection", {CLEAR_ON_MANAGER_START | CLEAR_ON_OFFROAD_TRANSITION, STRING}},
+    {"NavigationRecalculateRoutes", {CLEAR_ON_MANAGER_START, BOOL}},
 };
